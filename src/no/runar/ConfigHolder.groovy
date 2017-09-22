cd76933c758c5d635df48484992bfcddab8da8bf
runar <runar.opsahl@gmail.com>
2017-09-22 15:04:22 +0200

    Added steps to Config

--- src/no/runar/ConfigHolder.groovy
+++ src/no/runar/ConfigHolder.groovy
@@ -4,7 +4,7 @@ package no.runar
  * Created by runar on 22/09/2017.
  *
  */
-class ConfigHolder   {
+class ConfigHolder  implements Serializable {
     String text
     ConfigHolder() {
         text= " Config holder is initialized"
