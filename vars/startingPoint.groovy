0cf934f9aa03423a1587d3d4a6d97f1f1b9b31e4
runar <runar.opsahl@gmail.com>
2017-09-22 15:31:21 +0200

    parallel steps

--- vars/startingPoint.groovy
+++ vars/startingPoint.groovy
@@ -11,29 +11,24 @@ void call(){
     Config.steps=steps
     parallel one: {
         stage("Stage one"){
-            config1=r=Config.create("One")
+            config1=Config.create("One")
             echo "One Complete"
             echo config1.text
 
         }
     },two: {
         stage("Stage two"){
-            config2=r=Config.create("Two")
+            config2=Config.create("Two")
             echo "Two Complete"
             echo config2.text
 
         }
     },three: {
         stage("Stage three"){
-            config3=r=Config.create("Three")
+            config3=Config.create("Three")
             echo "Three Complete"
             echo config3.text
 
         }
-    }
-    node(){
-        echo config1.text
-        echo config2.text
-        echo config3.text
     }
 }
